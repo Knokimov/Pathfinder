ded3127b90fcaf93a3e27f55a0bfa4dd4bb64f75
Knokimov <Andreskov@yahoo.no>
2022-02-08 20:15:58 +0100

    changed Main

--- Main.java
+++ Main.java
@@ -28,39 +28,41 @@ class Main {
             System.exit(1);
         }
 
-        JFrame vindu = new JFrame("Labyrinth");
-        vindu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame window = new JFrame("Labyrinth");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
-        vindu.add(panel);
+        window.add(panel);
         panel.setPreferredSize(new Dimension(2000,2000));
-        JPanel labyrint = new JPanel();
-        labyrint.setLayout(new GridLayout(rows,columns));
+        JPanel labyrinth = new JPanel();
+        labyrinth.setLayout(new GridLayout(rows,columns));
         JButton[][] components = new JButton[rows][columns];
 
-        for(char[] x: l.grid){
-            for(char y: x){
-                JButton nr = new JButton();
-                nr.setPreferredSize(new Dimension(700/rows,700/columns));
-                components[y.row][y.column] = nr;
-                if(y instanceof SortRute){
-                    nr.setBackground(Color.BLACK);
-                }
-                else if (y instanceof HvitRute){
-                    nr.setBackground(Color.WHITE);
-                    nr.addActionListener(new Losninger(l,y,panel,components));
-                }
-                nr.setHorizontalAlignment(JLabel.CENTER);
-                nr.setVerticalAlignment(JLabel.CENTER);
-                nr.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                labyrint.add(nr);
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < columns; j++){
+			JButton nr = new JButton();
+			nr.setPreferredSize(new Dimension(700/rows,700/columns));
+			components[i][j] = nr;
+                
+            if(l.grid[i][j] == '#'){
+                nr.setBackground(Color.BLACK);
             }
+            else if (l.grid[i][j] == '.'){
+                nr.setBackground(Color.WHITE);
+                nr.addActionListener(new Losninger(l,l.grid[i][j],panel,components));
+            }
+
+            nr.setHorizontalAlignment(JLabel.CENTER);
+            nr.setVerticalAlignment(JLabel.CENTER);
+            nr.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            labyrinth.add(nr);
         }
-        panel.add(labyrint);
+        }
+        panel.add(labyrinth);
         JScrollPane scrollableArea = new JScrollPane(panel);  
         scrollableArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
         scrollableArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);   
-        vindu.getContentPane().add(scrollableArea);  
-        vindu.pack();
-        vindu.setVisible(true);
+        window.getContentPane().add(scrollableArea);  
+        window.pack();
+        window.setVisible(true);
     }
 }
