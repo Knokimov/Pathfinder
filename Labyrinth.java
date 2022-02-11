68e589fc7d39bada056aee7f5fed9bfef0f88d1b
Knokimov <Andreskov@yahoo.no>
2022-02-11 04:50:39 +0100

    does not crash anymore

--- Labyrinth.java
+++ Labyrinth.java
@@ -10,15 +10,16 @@ public class Labyrinth {
     int columns;
     ArrayList<ArrayList<Integer>> solutions;
 
-    public void addSolution(ArrayList<Integer> solution){
-        this.solutions.add(solution);
-    }
+    // public void addSolution(ArrayList<Integer> solution){
+    //     this.solutions.add(solution);
+    // }
 
 
-    Labyrinth(File fil) throws FileNotFoundException{
+    Labyrinth(File file) throws FileNotFoundException{
         this.file = file;
         this.solutions = new ArrayList<ArrayList<Integer>>();
         
+                    System.out.println("Char");
         try{
             Scanner scanner = new Scanner(file);
             String firstLine = scanner.nextLine();
@@ -29,14 +30,19 @@ public class Labyrinth {
             this.grid = new char[this.rows][this.columns];
             int rowNr = 0;
             
+            System.out.println("row and col" + this.rows + " " + this.columns);
             while(scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                // char[] row = line.split("(?!^)");
-                // String row = line.split("");
-                // char arr[] = str.toCharArray();
+                
+                char[] row = new char[line.length()];
+                for (int i = 0; i < line.length(); i++) {
+                    row[i] = line.charAt(i);
+                }
+
                 int columnNr = 0;
                 
                 for(char x: row){
+                    // System.out.println("Char" + x);
                     if(x == '.') {
                         if(columnNr == 0 || columnNr == columns - 1 || rowNr == 0 || rowNr == rows -1){
                             grid[rowNr][columnNr] =  'X';
--- Main.java
+++ Main.java
@@ -10,24 +10,27 @@ class Main {
         int columns = 1;
         Labyrinth l = null;
        
-        JFileChooser chooser = new JFileChooser();
-        int result = chooser.showOpenDialog(null);
-        if (result != JFileChooser.APPROVE_OPTION)
-            System.exit(1);
-            File f = chooser.getSelectedFile();
-            Scanner reader = null;
-        try {
-            l = new Labyrinth(f);            
-            reader = new Scanner(f);
-            String firstLine = reader.nextLine();
-            String[] rowsAndColumns = firstLine.split(" ");
-            rows = Integer.parseInt(rowsAndColumns[0]);
-            columns = Integer.parseInt(rowsAndColumns[1]);
+        // JFileChooser chooser = new JFileChooser();
+        // JFileChooser chooser = new JFileChooser("~/Desktop/Pathfinder/labyrinths");
+        // int result = chooser.showOpenDialog(null);
+        // if (result != JFileChooser.APPROVE_OPTION){
+            // System.exit(1);
+        // } else {
+                // File f = chooser.getSelectedFile();
+                File f = new File("/home/andrey/Desktop/Pathfinder/labyrinths/1.in");
+                Scanner reader = null;
+            try {
+                l = new Labyrinth(f);            
+                reader = new Scanner(f);
+                String firstLine = reader.nextLine();
+                String[] rowsAndColumns = firstLine.split(" ");
+                rows = Integer.parseInt(rowsAndColumns[0]);
+                columns = Integer.parseInt(rowsAndColumns[1]);
 
-        } catch (FileNotFoundException e) {
-            System.exit(1);
-        }
-
+            } catch (FileNotFoundException e) {
+                System.exit(1);
+            }
+        // }
         JFrame window = new JFrame("Labyrinth");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
@@ -37,26 +40,33 @@ class Main {
         labyrinth.setLayout(new GridLayout(rows,columns));
         JButton[][] components = new JButton[rows][columns];
 
+            System.out.println(l);
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < columns; j++){
 			JButton nr = new JButton();
 			nr.setPreferredSize(new Dimension(700/rows,700/columns));
 			components[i][j] = nr;
-                
+            // System.out.println("We are here");
+            
+            // System.out.println(l);
+            
             if(l.grid[i][j] == '#'){
                 nr.setBackground(Color.BLACK);
+                // System.out.println("We are her3");
             }
             else if (l.grid[i][j] == '.'){
                 nr.setBackground(Color.WHITE);
-                nr.addActionListener(new Losninger(l,l.grid[i][j],panel,components));
+                // nr.addActionListener(new Losninger(l,l.grid[i][j],panel,components));
+            // System.out.println("We are her4");
             }
 
+            // System.out.println("We are here2");
             nr.setHorizontalAlignment(JLabel.CENTER);
             nr.setVerticalAlignment(JLabel.CENTER);
             nr.setBorder(BorderFactory.createLineBorder(Color.BLACK));
             labyrinth.add(nr);
         }
-        }
+    }
         panel.add(labyrinth);
         JScrollPane scrollableArea = new JScrollPane(panel);  
         scrollableArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
